e12f9f1ad8a101420c4a8dda15391d284db456b7
Mauro Cipriani <mauro95br@gmail.com>
2018-05-10 23:58:07 +0200

    Fix lint space

--- src/com/martos/Main.java
+++ src/com/martos/Main.java
@@ -7,7 +7,6 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-
         JFrame frame = new JFrame("VAX Config Generator");
         frame.setContentPane(new App().main_panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
