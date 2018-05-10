aa506d37f576baa876c6d2e0151635691b20027a
Mauro Cipriani <mauro95br@gmail.com>
2018-05-10 23:57:25 +0200

    Added mac address

--- src/com/martos/gui/App.java
+++ src/com/martos/gui/App.java
@@ -17,6 +17,8 @@ public class App {
     private JLabel nvram_label;
     private JTextField data_disk_path;
     private JComboBox data_disk_type;
+    private JLabel eth_mac_label;
+    private JTextField eth_mac_text;
 
     private String[] cpu_modes = { "64", "128", "256", "512" };
 
@@ -65,6 +67,14 @@ public class App {
                     writer.println("set ts disable");
                     writer.println();
                     writer.println(";ETHERNET (Default)");
+
+                    if (!eth_mac_text.getText().isEmpty()) {
+                        writer.println("attach xq mac=" + eth_mac_text.getText());
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Mac address cannot be empty! Default option loaded.");
+                        writer.println("attach xq mac=08-00-2B-AA-BB-CC");
+                    }
+
                     writer.println("attach xq eth0");
                     writer.println();
                     writer.println(";END OF CONFIG");
