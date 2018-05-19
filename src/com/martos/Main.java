bb99120423432dba4df9207c7c5e75c1ad38a607
Mauro Cipriani <mauro95br@gmail.com>
2018-05-19 14:44:07 +0200

    Fixed warning

--- src/com/martos/Main.java
+++ src/com/martos/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("SIMH Config Generator");
         frame.setContentPane(new App().main_panel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
         frame.setResizable(false);
